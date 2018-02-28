2353d117c48dba27e0b98288a703395b823f1b6a
Peter Javorsky <tekk.sk@gmail.com>
2018-02-28 11:22:37 +0100

    First code

---
+++ AirQuality-Wifi.ino
@@ -0,0 +1,60 @@
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFiManager wifiManager;
+
+  wifiManager.setTimeout(180);
+  
+  if(!wifiManager.autoConnect("AirQualityAP")) {
+    Serial.println("Failed to connect and hit timeout.");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  } 
+
+  Serial.println("connected...yeey :)");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  
+  ArduinoOTA.begin();
+  
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  ArduinoOTA.handle();
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# air-quality-wifi
-An ESP8266 Wemos D1 Mini based air quality sensor node
+# Air Quality Wifi Node
+## An ESP8266 [Wemos D1 Mini](https://wiki.wemos.cc/products:d1:d1_mini) based air quality sensor node
+### Hardware needed
+- Wemos D1 Mini 
+- MQ-135 sensor
+
+![D1 Mini](img/d1_mini_v3.0.0.jpg)
+![MQ-135](img/mq-135.jpg)
+## Built with
+![VSCode](img/vscode.png)
+![Arduino](img/arduino.png)
+
+- [Visual Studio Code](https://code.visualstudio.com/)
+- VSCode [Arduino Plugin](https://marketplace.visualstudio.com/items?itemName=vsciot-vscode.vscode-arduino)
